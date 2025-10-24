f5d0d96f8aaf135ca1352c4ebd4591d6e6d7b372
Yancy Vance <yancyvance@gmail.com>
2025-10-24 06:58:50 -0400

    Create LastName_FirstName_Homework1.sql

---
+++ hw/LastName_FirstName_Homework1.sql
@@ -0,0 +1,77 @@
+-- CGS2545 Homework Assignment 1
+-- Student Name: <your complete name>
+-- File Name: LastName_FirstName_Homework1.sql
+
+-- NOTE: Put all your answers in between the START and END markers
+-- Any answers outside of these will be ignored.
+
+-- Use the correct database
+USE university_library;
+
+-- Q1 START
+
+
+-- Q1 END
+
+
+
+-- Q2 START
+
+
+-- Q2 END
+
+
+
+-- Q3 START
+
+
+-- Q3 END
+
+
+
+-- Q4 START
+
+
+-- Q4 END
+
+
+
+-- Q5 START
+
+
+-- Q5 END
+
+
+
+-- Q6 START
+
+
+-- Q6 END
+
+
+
+-- Q7 START
+
+
+-- Q7 END
+
+
+
+-- Q8 START
+
+
+-- Q8 END
+
+
+
+-- Q9 START
+
+
+-- Q9 END
+
+
+
+-- Q10 START
+
+
+-- Q10 END
